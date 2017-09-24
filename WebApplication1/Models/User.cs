9759df581ad0a9781c5f5d2570ce2bbbd593ba64
Dima <d.a.shimanov@gmail.com>
2017-09-24 19:21:16 +0700

    Добавил реализацию подключения к БД

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ WebApplication1/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
